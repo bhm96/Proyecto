b503499f8befe316290406832f4eab9e544a776d
Brayan Heredia <bherediam@est.ups.edu.ec>
2021-01-03 13:01:53 -0500

    Commit inicial

---
+++ script.py
@@ -0,0 +1,11 @@
+# Script de prueba para Git
+# Autor: Brayan Heredia <bherediam@est.ups.edu.ec>
+# Fecha: 03-01-2021
+# Versión: 1.0.0
+
+def main():
+    print("Hola mundo")
+
+if __name__ == '__main__':
+    main()
+
